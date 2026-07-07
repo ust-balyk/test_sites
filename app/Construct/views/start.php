fdd7c726028482af3bab49d895a5aebc1589d5e1
master <master@MacBook-Air-master.local>
2026-07-07 23:11:28 +0500

    new version

--- app/Construct/views/start.php
+++ app/Construct/views/start.php
@@ -257,8 +257,8 @@
                 data-id="<?= hsc($product['outer_id']) ?>"><i class="fa-solid fa-heart"></i>
               </button>
               <button class="btn btn-outline-secondary add-to-cart" style="background-color:transparent!important"
-                    data-id="<?= hsc($product['outer_id']) ?>">
-                <i class="fa-solid fa-cart-shopping 
+                    data-id="<?= hsc($product['outer_id']) ?>" title="カートに追加">
+                <i class="fa-solid fa-cart-shopping
                   <?= \App\Cart\Cart::hasProductInCart(hsc($product['outer_id']))?'in_cart':'' ?>"></i>
                 <div class="spinner-border loader d-none" 
                     style="width:2.2rem;height:2.2rem;margin-left:0.8rem;color:#90cdfb" role="status">
